f574905fd20fe939ad22ba0950196e05dfa73f26
Sta2yan <s2_stas@mail.ru>
2022-02-26 20:09:21 +0300

    Добавьте файлы проекта.

---
+++ CrystalStore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myMoney = 100;
+            int myCrystal = 0;
+            int priceCrystal = 10;
+            int countBuyCrystal;
+
+            Console.Write("Введите кол-во кристаллов, которое хотели бы купить: ");
+            countBuyCrystal = Convert.ToInt32(Console.ReadLine());
+
+            myMoney -= priceCrystal * countBuyCrystal;
+            myCrystal += countBuyCrystal;
+
+            Console.WriteLine($"\nДеньги: {myMoney}" +
+                              $"\nКристаллы: {myCrystal}");
+        }
+    }
+}
